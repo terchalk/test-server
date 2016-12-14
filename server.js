6ef5909ca98ed54fee9b8f6195e9829e9aeaf868
Terence <terencechalk@hotmail.com>
2016-12-14 10:48:39 +0800

    test

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function () {
+	console.log("Server started at " + port);
+});
